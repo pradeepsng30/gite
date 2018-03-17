e8bef272c71cd0026da125e1466c85172f9f4d8e
Pradeep Singh <pradeepsng30@yahoo.com>
2018-03-17 22:59:34 +0530

    add content for error scenarios

--- content.js
+++ content.js
@@ -2,5 +2,8 @@ module.exports = {
     'usage': "\nusage: egit  [options] <command> [<args>]",
     'optionsHeader': '\noptions:\n',
     'commandsHeader': '\ncommands:\n',
-    'helpFooter': '\n For more detailed help for a command run `egit -h <command>`\n '
+    'helpFooter': '\n For more detailed help for a command run `egit -h <command>`\n ',
+    'cmdNotFound': 'is not a valid command.',
+    'relatedCmds': '\nAre you trying to use these commands?',
+    'errFooter': '\n Try \'egit -h\' for list of all available commands.\n'
 };
