a7983aeb4b3102f00de7db161dbea1bbfbb165e5
Pradeep Singh <pradeepsng30@yahoo.com>
2018-03-18 11:28:27 +0530

    remove unwanted files

--- output.js
+++
@@ -1,8 +0,0 @@
-const     chalk = require("chalk");
-module.exports = (error, stdout, stderr) => {
-    if (error) console.log(chalk.red.bold.underline("exec error: ") + error);
-    if (stdout) console.log(chalk.green.bold.underline("Result:\n") + stdout);
-    if (stderr) console.log(chalk.red("Error: ") + stderr);
-};
-
-//module.exports = output(); 
