f2fc020416b548ca5b36cd4db5bffda5dade94fa
Pradeep Singh <pradeepsng30@yahoo.com>
2018-03-18 16:03:40 +0530

    improve long desc for help

--- help.js
+++ help.js
@@ -1,13 +1,40 @@
 'use strict';
 const content = require('./content');
 
+let getTextFromArray = function(arr, prefix){
+    let text = "";
+    arr.forEach(el => {
+        text = text + '\n' + prefix + el.toString()
+    })
+    return text;
+};
+
+let getLongDesc = function (element) {
+let longDesc = element.longDesc;
+let text ="";
+    if (!longDesc){
+        return text;
+    } 
+    if (longDesc.required && longDesc.required.length > 0){
+         text = text +'\nRequired Arguments:' + getTextFromArray(longDesc.required, '\t');
+    }
+    if (longDesc.optional && longDesc.optional.length > 0){
+        text = text +'\n\nOptional Arguments:' + getTextFromArray(longDesc.optional, '\t');
+    }
+    if (longDesc.samples && longDesc.samples.length > 0){
+        text = text +'\n\nSamples:' + getTextFromArray(longDesc.samples, `\t egit ${element.cmd} `);
+    }
+    text = text +'\n\n';
+    return text;
+}
+
 let showHelpForCommand = function (element, showFull) {
     showFull = showFull || false;
     let result ="";
     if(showFull) {
         result = "\n" + "usage:\t egit " + element.cmd +  (element.args ? " " + element.args : "") + "\n\n";
-        result = result + "Description:\t" + element.desc + "\n";
-        result = result + (element.longDesc ? element.longDesc: "");
+        result = result + "Description:\n" + element.desc + "\n";
+        result = result + (element.longDesc ? getLongDesc(element): "");
     }
     else {
         result = "\t" + padding(element.cmd.split(' ')[0],30) + "\t" + element.desc;
