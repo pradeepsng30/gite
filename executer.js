8118c08b3f75af7cd39e8ee0dbe19604720b1742
Pradeep Singh <pradeepsng30@yahoo.com>
2018-03-18 11:37:49 +0530

    executor

--- executer.js
+++ executer.js
@@ -1,5 +1,17 @@
-const child = require('child_process')
+'use strict';
+const cp = require('child_process')
 
-module.exports = (cmd,output) => {
-    child.exec(cmd, output);
+//module.exports = (cmds) => {
+let exec = function(cmds) { 
+    try {
+    cmds.forEach(cmd =>{
+        console.log(cp.execSync(cmd).toString())
+    });
+    } catch (e) {
+        console.log('\x1b[31m%s\x1b[0m', 'Error occured');
+        console.log(e.toString())
+    }
 }
+module.exports = exec;
+
+// exec(['git status', 'git branch', 'git help']);
