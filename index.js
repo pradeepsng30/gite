3fcea6111055122f63a26cf1da76f36a567d5b04
Pradeep Singh <pradeepsng30@yahoo.com>
2018-03-24 16:08:53 +0530

    lint fixes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 'use strict';
 const input_args = require('yargs').argv;
 
-const args = input_args._
+const args = input_args._;
 const options = input_args;
 const command = args[0];
 
@@ -15,53 +15,45 @@ const execute = require('./executer');
 
 let getRelatedCommands = (command) => {
     return commandList.filter(element => {
-       return element.cmd.split(' ')[0].indexOf(command) !== -1 && (command.length >=4)
+        return element.cmd.split(' ')[0].indexOf(command) !== -1 && (command.length >=4);
     });
-}
+};
 
 let getCommandConfig = function (command) {
     return commandList.filter(element =>{
         return element.cmd === command;
     })[0];
-}
-
-let mapAndExecute = function (element) {
-    let cmdsToexec = element.execCommands.map(cmdObj => {
-        return
-    });
-}
+};
 
 let validateAndParse = function (element) {
     let result = {};
     if (element.args) {
-        //console.log(args);
-        let requiredArgs = element.args.split(' ').forEach( (arg,i) => {
-            let argName = '';
+        element.args.split(' ').forEach( (arg,i) => {
+            let argName ;
             if(arg.length < 3) {
-                throw Error("invalid arg list in config");
+                throw Error('invalid arg list in config');
             }
             argName = arg.slice(1,-1);
-            if(arg[0] == '<' && arg[arg.length-1] == '>') {
+            if(arg[0] === '<' && arg[arg.length-1] === '>') {
                 if(i+1 >= args.length) {
                     throw Error(`missing required argument: ${argName}`);
                 }
                 result[argName] = args[i+1];
-            } else if(arg[0] == '[' && arg[arg.length-1] == ']') {
+            } else if(arg[0] === '[' && arg[arg.length-1] === ']') {
                 if(i+1 < args.length) {
                     result[argName] = args[i+1];
                 }
             }
             else {
-                throw Error("invalid arg list in config");
+                throw Error('invalid arg list in config');
             }
         });
     }
     return result;
-}
+};
 
 
 let injectValues = function (str, values) {
-    // console.log(values);
     let pattern = /{{.*?}}/g ;
     let pattern2 = /\$.*\$/g;
     let textsToreplace = str.match(pattern);
@@ -70,29 +62,27 @@ let injectValues = function (str, values) {
         let varName = varString.slice(1,-1);
         let varVal = values[varName];
         if(varVal && varVal.indexOf(' ') > -1) {
-            varVal = "\'" + varVal + "\'"; 
+            varVal = '\'' + varVal + '\''; 
         }
-        let replacement = (varVal === undefined) ? "" : textToreplace.slice(2,-2).replace(varString, varVal.toString());
-        // console.log(textToreplace, varString, varName, varVal, replacement);
-        str = str.replace(textToreplace, replacement); 
+        let replacement = (varVal === undefined) ? '' : textToreplace.slice(2,-2).replace(varString, varVal.toString());
+        str = str.replace(textToreplace, replacement);
     });
     return str.replace(/\s+/g,' ').trim();
-}
+};
 
 let getNewCmds = function (element, argObj) {
     return element.execCommands.map(cmdObj => {
-        let cmd = cmdObj.newcmd + " " + cmdObj.args;
-        // console.log("PPP", cmd);
+        let cmd = cmdObj.newcmd + ' ' + cmdObj.args;
         return injectValues(cmd, argObj);
     });
-}
+};
 
 let showCmds = function (cmds) {
     console.log(content.showCmdsHeader);
     cmds.forEach(cmd => {
-        console.log("\t" + cmd);
+        console.log('\t' + cmd);
     });
-}
+};
 
 let main  = function () {
     if (options.version || options.v) {
@@ -104,20 +94,20 @@ let main  = function () {
                 if (options.h){ 
                     help.showHelpForCommand(commandConfig, true);
                 } else {
-                let cmdsToExec = getNewCmds(commandConfig, validateAndParse(commandConfig));
-                if(options.s || options.show){
-                    showCmds(cmdsToExec);
-                } else {
-                    execute(cmdsToExec);            
-                }
+                    let cmdsToExec = getNewCmds(commandConfig, validateAndParse(commandConfig));
+                    if(options.s || options.show){
+                        showCmds(cmdsToExec);
+                    } else {
+                        execute(cmdsToExec);            
+                    }
                 }
             }
             else {
-                console.log("'" + command + "'", content.cmdNotFound);
+                console.log('\'' + command + '\'', content.cmdNotFound);
                 let relatedCommands = getRelatedCommands(command);        
                 if(relatedCommands && relatedCommands.length > 0) {
                     console.log(content.relatedCmds);
-                    help.showHelpForCommands(relatedCommands)
+                    help.showHelpForCommands(relatedCommands);
                 }
                 console.log(content.errFooter);
             }
@@ -125,13 +115,13 @@ let main  = function () {
             help.showHelpGlobal(config);
         }
     }
-}
+};
 
 
 try {
     main();
 } catch (e) {
     console.log('\x1b[31m%s\x1b[0m', 'Error occured');
-    console.log(e.toString())
+    console.log(e.toString());
     console.log(content.errFooter);
 }
